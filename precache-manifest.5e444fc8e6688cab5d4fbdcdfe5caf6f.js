23d8af0df53a610a60f7fc1ef2c9a7ada8ca756b
Naveenkumar <65083187+Naveen112001@users.noreply.github.com>
2022-08-04 11:38:51 +0530

    Updates

---
+++ precache-manifest.5e444fc8e6688cab5d4fbdcdfe5caf6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd51ed7a9fec65130af4",
+    "url": "/my-app/static/css/main.261458c2.chunk.css"
+  },
+  {
+    "revision": "cd51ed7a9fec65130af4",
+    "url": "/my-app/static/js/main.cd51ed7a.chunk.js"
+  },
+  {
+    "revision": "b4580c6e423a71b044a8",
+    "url": "/my-app/static/js/1.b4580c6e.chunk.js"
+  },
+  {
+    "revision": "3a9f14da87c4f0131618",
+    "url": "/my-app/static/css/2.fcea642f.chunk.css"
+  },
+  {
+    "revision": "3a9f14da87c4f0131618",
+    "url": "/my-app/static/js/2.3a9f14da.chunk.js"
+  },
+  {
+    "revision": "42a36bb0e4851835b079",
+    "url": "/my-app/static/js/runtime~main.42a36bb0.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d0dad38c90da8bd9c0e8051e9eb90878",
+    "url": "/my-app/index.html"
+  }
+];
