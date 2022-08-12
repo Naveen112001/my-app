ee00eb0d619031cd5f48f7a121e311543f2d3719
Naveenkumar <65083187+Naveen112001@users.noreply.github.com>
2022-08-12 18:25:41 +0530

    upload

---
+++ src/Components/Admin_folder/Books/Books_landing.js
@@ -0,0 +1,11 @@
+// JavaScript source cod
+import axios from 'axios';
+import pic from './books.jpg';
+import React, { Fragment, useState, useEffect } from "react";
+import * as ReactDOM from 'react-dom';
+import { useNavigate } from "react-router-dom";
+function Books_landing()
+{
+
+}
+export default Books_landing;
